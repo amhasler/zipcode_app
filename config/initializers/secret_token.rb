9c84e3528332bb53b7e9e9c1dcc90eb62686a1ca
a.m. hasler <amhasler@gmail.com>
2014-05-20 13:10:52 -0400

    app created, gemfile fixed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZipcodeApp::Application.config.secret_key_base = '17bc7063a471175b14203acb7bb672b33d8a53daaada823b7e1f6d0cfc59b04cc98b35c5caea874091091ce461d283aa7430d52aff168df3b61b49dd4d1fd763'
